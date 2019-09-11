8ba3a72d5c76de76eb9e005af01ba53f003a9bd6
sabdi21 <sabdi21@hotmail.com>
2019-09-11 14:24:59 -0700

    added user model

---
+++ index.js
@@ -0,0 +1,20 @@
+//Require needed packages
+let express = require('express')
+let cors = require('cors')
+let morgan = require('morgan')
+
+//instantiate app
+let app = express()
+
+//set-up middleware
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '30mb' }))
+
+
+app.get('*', (req, res) => {
+    res.status(404).send({ message: 'Not Found' })
+})
+
+app.listen(process.env.PORT || 4000)
